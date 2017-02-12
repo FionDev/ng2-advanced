158ec2767d773792a43796434c73fd49db9d5a14
Fion <fiondev7@gmail.com>
2017-02-12 14:34:47 +0800

    09 練習路由參數的設定、注入路由相關元件、設定 dashboard 可以導覽到 cards 元件並帶 :type 路由參數過去

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ import {DashboardComponent} from './dashboard/dashboard.component'
 import {CardsComponent} from './cards/cards.component'
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'cards',     component: CardsComponent },
+  { path: 'cards/:type',     component: CardsComponent },
   { path:'charts',
     children:[
       //{ path:'',redirectTo:'flot',pathMatch:'full'},
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute,Router} from '@angular/router';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  type:string;
+
+  constructor(private router:Router,private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.type=this.route.snapshot.params['type'];
   }
 
 }
