27f580da3a7e52e4e5c51c9b92ea7084e8e55920
Fion <fiondev7@gmail.com>
2017-02-12 16:22:15 +0800

    13 練習兩種不同的可選參數用法 (Optional Parameters)

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -9,7 +9,7 @@ import {ActivatedRoute,Router} from '@angular/router';
 export class CardsComponent implements OnInit {
 
   type:string;
-
+  name:string;
   constructor(private router:Router,private route:ActivatedRoute) { }
 
   //只有在創建時會執行一次
@@ -17,13 +17,34 @@ export class CardsComponent implements OnInit {
     //此用法只是適用value不會在被修改
     //this.type=this.route.snapshot.params['type'];
     //改RXJS採非同步 訂閱者模式 一旦值修改則會通知更新
+    //取得參數
     this.route.params.subscribe( params => {
        this.type = params['type'];
+      console.log('Matrix: ' + params['name']);
      });
+     //取得參數
+     this.route.queryParams.subscribe( params => {
+       console.log('QueryString: ' + params['name']);
+     })
   }
 
   updateCards(type)
   {
     this.router.navigateByUrl('/cards/' + type);
   }
+
+  //QueryString傳參數 程式寫法
+  goQueryString() {
+     this.router.navigate(['/cards', 100], {
+       queryParams: {
+         name: 'QueryString-by-Code'
+       }
+     })
+   }
+  //Matrix傳參數 程式寫法
+   goMatrix() {
+     this.router.navigate(['/cards', 100, {
+         name: 'Matrix-by-Code',
+       }]);
+   }
 }
