f8ecb052b02a44ecd4fba867bd67595abc01fb0f
Fion <fiondev7@gmail.com>
2017-02-12 14:49:22 +0800

    10 練習在相同路由之間切換不同參數 ( 改用 this.route.params 訂閱參數變更 )

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -12,8 +12,18 @@ export class CardsComponent implements OnInit {
 
   constructor(private router:Router,private route:ActivatedRoute) { }
 
+  //只有在創建時會執行一次
   ngOnInit() {
-    this.type=this.route.snapshot.params['type'];
+    //此用法只是適用value不會在被修改
+    //this.type=this.route.snapshot.params['type'];
+    //改RXJS採非同步 訂閱者模式 一旦值修改則會通知更新
+    this.route.params.subscribe( params => {
+       this.type = params['type'];
+     });
   }
 
+  updateCards(type)
+  {
+    this.router.navigateByUrl('/cards/' + type);
+  }
 }
