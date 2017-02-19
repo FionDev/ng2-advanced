95c716a5f3dedc21e892cba92eed497434f2734e
Fion <fiondev7@gmail.com>
2017-02-19 08:58:01 +0800

    將 DashboardComponent 需要的初始化程式轉成 TypeScript 版本

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute,Router} from '@angular/router';
+//將 DashboardComponent 需要的初始化程式轉成 TypeScript 版本
+import { initDashboard } from '../shared/app-init';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,6 +12,7 @@ export class DashboardComponent implements OnInit {
   constructor(private router:Router,private route:ActivatedRoute) { }
 
   ngOnInit() {
+    initDashboard();
   }
 
   goCards(type)
