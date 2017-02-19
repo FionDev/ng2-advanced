9a250fb328d4558b6435b86af8defe863d84dcdc
Fion <fiondev7@gmail.com>
2017-02-19 12:06:56 +0800

    20 練習透過 FormArray 搭配 *ngFor 產生動態表單結構

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Validators, FormGroup,  FormBuilder} from '@angular/forms';
+
+import {FormArray, Validators,  FormGroup,   FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-reactiveform',
   templateUrl: './reactiveform.component.html',
@@ -8,13 +9,23 @@ import {Validators, FormGroup,  FormBuilder} from '@angular/forms';
 export class ReactiveformComponent implements OnInit {
 
   form: FormGroup;
-  constructor(private fb: FormBuilder)
-  {
-     this.form = this.fb.group({ 'name': ['Will', Validators.required]});
-   }
+   constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      'name': ['Fion', [Validators.required, Validators.minLength(3)]],
+      'group1': this.fb.array([
+        this.fb.control('Fion1', Validators.required),
+        this.fb.control('Fion2', Validators.required),
+        this.fb.control('Fion3', Validators.required),
+        this.fb.control('Fion4', Validators.required)
+      ])
+    });
+  }
 
   ngOnInit() {
     this.form.addControl('email', this.fb.control('default@example.com', Validators.required));
+    let group1: FormArray = <FormArray>this.form.controls['group1'];
+    group1.insert(group1.length, this.fb.control('Will 5'));
+
   }
  getFieldInvalid(fieldName) {
      return this.form.controls[fieldName].invalid;
